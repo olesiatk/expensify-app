14f94bf2ce6ad84b69da308d6662c39de16fe085
okazanivska <okazanivska@leobit.com>
2022-10-27 23:08:59 +0300

    feat: Setup devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,9 @@ module.exports = (env) => {
     })],
     entry: './src/app.js',
     output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
+        path: path.join(__dirname, 'public', 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist/'
     },
     module: {
         rules: [{ 
